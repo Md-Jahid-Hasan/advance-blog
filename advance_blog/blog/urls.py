f87710f231eafb011d8323b318457353b338a500
jahid4 <jahid@impelitsolutions.com>
2023-10-21 12:19:36 +0600

    updated blog api

--- advance_blog/blog/urls.py
+++ advance_blog/blog/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
-from .views import BlogCreateView
+from django.urls import path
+from blog.api.views import BlogCreateView
+from .views import CreateBlogTemplate
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view())
+    path('create/', BlogCreateView.as_view(), name="blog_create_api"),
+    path('blog-create/', CreateBlogTemplate.as_view(), name="blog_create_template")
 ]
--- advance_blog/blog/views.py
+++ advance_blog/blog/views.py
@@ -1,32 +1,5 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
-
-from .models import Blog, BlogImage, BlogSection
-from .serializers import BlogSerializer
-
-from user.services import UserService
+from django.views.generic.base import TemplateView
 
 
-class BlogCreateView(generics.CreateAPIView):
-    serializer_class = BlogSerializer
-    permission_classes = (AllowAny, )
-
-    def create(self, request, *args, **kwargs):
-        data = {"error": "something went wrong"}
-        author = request.data.get('user')
-
-        if all(key in author for key in ['email', 'name']):
-            return super().create(request, *args, **kwargs)
-        else:
-            data = {"error": "Name/email not provided"}
-        return Response(data=data, status=HTTP_400_BAD_REQUEST)
-
-    def perform_create(self, serializer):
-        if self.request.user.is_anonymous:
-            author = self.request.data.get('user')
-            author = UserService.get_or_create_user(author)
-            serializer.save(author=author)
-        else:
-            serializer.save(self.request.user)
+class CreateBlogTemplate(TemplateView):
+    template_name = 'blog/create.html'
